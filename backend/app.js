71d9bd3698121ff0f78f0a6b667ff1a38914fa71
Digvijay7310 <kumardigvijay377@gmail.com>
2025-06-15 15:51:46 +0530

    design login page

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,40 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
+import userRoutes from './routes/user.routes.js'
 
 const app = express()
 
 
 // import middlewares
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true
 }))
+
+//Middlewares
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.json({ limit: "16kb" }))
-app.use(cookieParser())
+app.use((err, res, req, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+        ...app(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    })
+})
+
+//Static file serving
+app.use("/public", express.static("public"))
 
 
-//import routes
 
-import userRoutes from './routes/user.routes.js'
 
 app.use("/api/v1/users", userRoutes)
 
